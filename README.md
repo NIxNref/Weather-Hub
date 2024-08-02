8323175a8f4b0db88974dfe2e909761790aa4d74
NIxNref <pnugroho040@gmail.com>
2024-08-02 10:57:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_arkas_3
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/weather_arkas_3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weather_arkas_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
